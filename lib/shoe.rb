5ac22ea9b7575349e714d52e1235490e00b9138c
Valerie Moore <periwinkle17@outlook.com>
2018-06-17 18:22:24 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,6 +3,11 @@ class Shoe
   attr_reader :brand
   
   BRANDS = []
+  
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+  end
 
   def initialize(brand)
     @brand = brand
@@ -13,8 +18,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-  end
+  
 end
