32b71612c75aa1c48f68135a70e5b5cb1d5f5917
Valerie Moore <periwinkle17@outlook.com>
2018-06-17 18:23:36 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,14 +4,14 @@ class Shoe
   
   BRANDS = []
   
-  def initialize=(brand)
+  def brand=(brand)
     @brand = brand
     BRANDS << brand
   end
 
-  # def initialize(brand)
-  #   @brand = brand
-  # end
+  def initialize(brand)
+    @brand = brand
+  end
 
   def cobble
     self.condition = "new"
