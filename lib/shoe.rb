a6f8eb8193b1afb5ac6b3deae80f2aa746d4438a
Valerie Moore <periwinkle17@outlook.com>
2018-06-17 18:17:48 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,11 +4,6 @@ class Shoe
   
   BRANDS = []
 
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-  end
-  
   def initialize(brand)
     @brand = brand
   end
@@ -18,5 +13,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+  end
 end
