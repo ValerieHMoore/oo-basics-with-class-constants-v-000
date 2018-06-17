2d4cb8ff2d977fe7151b2e50431fcf40d08bb975
Valerie Moore <periwinkle17@outlook.com>
2018-06-17 18:03:20 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -19,21 +19,3 @@ class Shoe
   end
 
 end
-
-
-  GENRES = []
-
-  def initialize(title)
-    @title = title
-  end
-
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-  
-  def genre=(genre)
-    @genre = genre
-    GENRES << genre
-  end
-end
-
